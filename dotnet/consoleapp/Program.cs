67bdd4f9ea0984debdc212c1f9a3a80b4ee83fec
Leniel Luzardo <lenielluzardo.dev@gmail.com>
2025-05-06 12:08:43 -0300

    imp challenge | dotnet | check even/odd number

--- dotnet/consoleapp/Program.cs
+++ dotnet/consoleapp/Program.cs
@@ -3,25 +3,11 @@ using challenge.basic;
 using challenge.easy;
 
 Console.WriteLine("Hello, World!");
-
-
 Console.WriteLine(Environment.NewLine);
 Console.WriteLine("Please enter a number in.");
-string input = Console.ReadLine();
 
-if (string.IsNullOrEmpty(input))
-{
-  Console.WriteLine
-  (@"
-    You need to provide a number.
-    Try again.
-  ");
-}
 
-int value = int.Parse(input);
-
-int result = SumOfDigitsOfNumber.Start(value);
-
-Console.WriteLine("The result of the sum is: {0}", result);
+SumOfDigitsOfNumber.Start();
+CheckEvenNumbers.Start();
 
 Console.ReadLine();
